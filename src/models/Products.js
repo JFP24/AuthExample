4dcd590cd60a6f5706556ee03d0c4558fe7c3722
JUAN <workpulgarin@gmail.com>
2022-08-01 20:52:52 -0500

    Models

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require("sequelize");
+//desde modelos haciendo cambios para pull
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
